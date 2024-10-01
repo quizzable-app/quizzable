37b5a34c7dd8f1a65d0e918344f906223e7afc1a
Tim Shaker <tim@tmshkr.com>
2024-10-01 10:38:59 -0700

    update deploy

--- apps/web/deploy.sh
+++ apps/web/deploy.sh
@@ -1,10 +1,16 @@
 #! /bin/bash -e
+
+if [ -z "$VERCEL_TOKEN" ]; then
+    echo "VERCEL_TOKEN is not set"
+    exit 1
+fi
+
 cd ../../
-vercel link --yes --project quizzable
-vercel build --yes
+vercel link --yes --project quizzable --token $VERCEL_TOKEN
+vercel build --yes --token $VERCEL_TOKEN
 
 if [ "$GITHUB_REF_NAME" == "main" ]; then
-    vercel deploy --prebuilt --prod
+    vercel deploy --prebuilt --prod --token $VERCEL_TOKEN
 else
-    vercel deploy --prebuilt
+    vercel deploy --prebuilt --token $VERCEL_TOKEN
 fi
