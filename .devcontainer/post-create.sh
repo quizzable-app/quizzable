7ab99d5c53ae207537575849faf4f7248e00298d
Tim Shaker <tim@tmshkr.com>
2024-10-04 09:55:43 -0700

    update post-create.sh

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -e
 
-# sudo apt-get update
-# sudo apt-get install -y tmux
+sudo apt-get update
+sudo apt-get install -y tmux
 
 # Install Dependencies
-# npm i -g firebase-tools
-# npm install
+npm i -g firebase-tools
+npm install
 
 echo "DATABASE_URL=postgresql://admin:LocalPasswordOnly@postgres/quizzable-dev?schema=public" >.env
 
@@ -13,7 +13,7 @@ echo "DATABASE_URL=postgresql://admin:LocalPasswordOnly@postgres/quizzable-dev?s
 echo "PRISMA_QUERY_ENGINE_LIBRARY=../../node_modules/.prisma/client/libquery_engine-debian-openssl-1.1.x.so.node" >>apps/functions/.env
 
 echo "DATABASE_URL=postgresql://admin:LocalPasswordOnly@postgres/quizzable-dev?schema=public" >apps/web/.env
-echo "FIREBASE_AUTH_EMULATOR_HOST=127.0.0.1:9099" >apps/web/.env
+echo "FIREBASE_AUTH_EMULATOR_HOST=127.0.0.1:9099" >>apps/web/.env
 echo "NEXT_PUBLIC_FIREBASE_APP_ID=$QUIZZABLE_FIREBASE_APP_ID" >>apps/web/.env
 echo "NEXT_PUBLIC_FIREBASE_API_KEY=$QUIZZABLE_FIREBASE_API_KEY" >>apps/web/.env
 echo "NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN=127.0.0.1:9099" >>apps/web/.env
