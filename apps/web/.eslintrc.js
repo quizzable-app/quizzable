763335ee4d66453f3f78993de2188cd49fcf07a5
Tim Shaker <tim@tmshkr.com>
2024-10-05 17:13:11 -0700

    update .eslintrc.js

--- apps/web/.eslintrc.js
+++ apps/web/.eslintrc.js
@@ -2,6 +2,10 @@
 module.exports = {
   root: true,
   extends: ["@repo/eslint-config/next.js"],
+  rules: {
+    "@next/next/no-img-element": "off",
+    "turbo/no-undeclared-env-vars": "off",
+  },
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: true,
