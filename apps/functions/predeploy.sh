aa8992f9359780a169cb832f5a6c5f707a9894bc
Tim Shaker <tim@tmshkr.com>
2024-10-08 12:17:19 -0700

    update firebase predeploy

--- apps/functions/predeploy.sh
+++ apps/functions/predeploy.sh
@@ -1,22 +1,17 @@
 #!/bin/bash -e
 
-if [ ! -f .env ]; then
-  echo "Missing: .env"
+if [ -f .env ]; then
+  echo ".env should not be present"
   exit 1
 fi
-
-source .env
 
 if [ -z "$DATABASE_URL" ]; then
   echo "Missing: DATABASE_URL"
   exit 1
 fi
 
-target_engine="dist/libquery_engine-debian-openssl-3.0.x.so.node"
-if [ "$PRISMA_QUERY_ENGINE_LIBRARY" != "$target_engine" ]; then
-  echo "PRISMA_QUERY_ENGINE_LIBRARY should be set to $target_engine"
-  exit 1
-fi
+echo "DATABASE_URL=$DATABASE_URL" >.env
+echo "PRISMA_QUERY_ENGINE_LIBRARY=dist/libquery_engine-debian-openssl-3.0.x.so.node" >>.env
 
 mkdir -p dist
 cp ../../node_modules/prisma/libquery_engine-debian-openssl-3.0.x.so.node dist
