8f7bf436084e701ba3aa8f07919632bed40ef81c
Tim Shaker <tim@tmshkr.com>
2024-10-08 11:16:26 -0700

    update functions test setup

--- apps/functions/globalSetup.ts
+++ apps/functions/globalSetup.ts
@@ -1,8 +1,8 @@
 import type { GlobalSetupContext } from "vitest/node";
 
 export default function setup({ provide }: GlobalSetupContext) {
-  const { DATABASE_URL, TEST_DATABASE_URL } = process.env;
-  if (!(DATABASE_URL || TEST_DATABASE_URL)) {
-    throw new Error("DATABASE_URL or TEST_DATABASE_URL must be set");
+  const { TEST_DATABASE_URL } = process.env;
+  if (!TEST_DATABASE_URL) {
+    throw new Error("TEST_DATABASE_URL must be set");
   }
 }
