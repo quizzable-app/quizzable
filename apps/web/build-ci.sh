61d1d688221092af80234f174b594784f6e4aa5a
Tim Shaker <tim@tmshkr.com>
2024-10-09 09:54:34 -0700

    update build-ci.sh

--- apps/web/build-ci.sh
+++ apps/web/build-ci.sh
@@ -7,4 +7,9 @@ fi
 
 cd ../../
 vercel link --yes --project quizzable --token $VERCEL_TOKEN
-vercel build --yes --token $VERCEL_TOKEN
+
+if [ "$ENVIRONMENT" == "production" ]; then
+    vercel build --yes --token $VERCEL_TOKEN --prod
+else
+    vercel build --yes --token $VERCEL_TOKEN
+fi
